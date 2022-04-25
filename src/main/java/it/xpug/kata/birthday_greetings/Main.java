7e1109c0b2f2313d4941c72f43bed40731fcea6d
David Drugeon-Hamon <zebeurton@gmail.com>
2022-04-25 11:42:55 +0200

    feat: Add AWS Lambda function

--- src/main/java/it/xpug/kata/birthday_greetings/Main.java
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.java
@@ -3,13 +3,19 @@ package it.xpug.kata.birthday_greetings;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.text.ParseException;
 
 public class Main {
 
-	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
+	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException, URISyntaxException {
+		URL resource = Main.class.getResource("/employee_data.txt");
+		String filename = Paths.get(resource.toURI()).toFile().getAbsolutePath();
+
 		BirthdayService service = new BirthdayService();
-		service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
+		service.sendGreetings(filename, new XDate(), "localhost", 25);
 	}
 
 }
