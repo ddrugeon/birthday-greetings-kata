1132415221aca8cec710c65057f0557c110ba8a1
David Drugeon-Hamon <zebeurton@gmail.com>
2022-04-27 11:30:42 +0200

    refactor: lambda with hexagonal architecture

--- src/main/java/it/xpug/kata/birthday_greetings/application/BirthdayGreetingServiceImpl.java
+++ src/main/java/it/xpug/kata/birthday_greetings/application/BirthdayGreetingServiceImpl.java
@@ -2,12 +2,11 @@ package it.xpug.kata.birthday_greetings.application;
 
 import it.xpug.kata.birthday_greetings.domain.entities.Employee;
 import it.xpug.kata.birthday_greetings.domain.vo.XDate;
+import it.xpug.kata.birthday_greetings.infrastructure.exceptions.NotificationException;
 import it.xpug.kata.birthday_greetings.infrastructure.spi.EmployeeRepository;
 import it.xpug.kata.birthday_greetings.infrastructure.spi.Notifier;
-import it.xpug.kata.birthday_greetings.infrastructure.exceptions.NotificationException;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BirthdayGreetingServiceImpl implements BirthdayGreetingService {
@@ -22,10 +21,12 @@ public class BirthdayGreetingServiceImpl implements BirthdayGreetingService {
 
 	@Override
 	public void sendGreetings(String sender, LocalDate date) throws NotificationException {
-		var employees = employeeRepository.findEmployeesWithBirthdayAt(new XDate(date));
+		if (notifier != null) {
+			var employees = employeeRepository.findEmployeesWithBirthdayAt(new XDate(date));
 
-		for (Employee e : employees) {
-			notifier.sendMessage(sender, e);
+			for (Employee e : employees) {
+				notifier.sendMessage(sender, e);
+			}
 		}
 	}
 
--- src/main/java/it/xpug/kata/birthday_greetings/infrastructure/api/BirthdayLambdaAdapter.java
+++ src/main/java/it/xpug/kata/birthday_greetings/infrastructure/api/BirthdayLambdaAdapter.java
@@ -4,9 +4,12 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import it.xpug.kata.birthday_greetings.application.BirthdayGreetingService;
+import it.xpug.kata.birthday_greetings.application.BirthdayGreetingServiceImpl;
 import it.xpug.kata.birthday_greetings.domain.entities.Employee;
 import it.xpug.kata.birthday_greetings.domain.vo.XDate;
 import it.xpug.kata.birthday_greetings.infrastructure.api.models.EmployeeJSONModel;
+import it.xpug.kata.birthday_greetings.infrastructure.spi.EmployeeCSVFileRepository;
+import it.xpug.kata.birthday_greetings.infrastructure.spi.EmployeeRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
@@ -15,7 +18,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,34 +35,19 @@ public class BirthdayLambdaAdapter {
 
     @Logging(logEvent = true, samplingRate = 0.7)
     public String handleRequest(String input, Context context) {
-        List<Employee> employees = new ArrayList<>();
-        BufferedReader in = null;
-        String str = "";
         try {
-            in = new BufferedReader(new FileReader("employee_data.txt"));
-            str = in.readLine(); // skip header
-            while ((str = in.readLine()) != null) {
-                log.info("Parsing new line");
-                String[] employeeData = str.split(", ");
-                log.info("Current Employee data {}", employeeData);
-                Employee employee = new Employee(employeeData[1], employeeData[0], new XDate(employeeData[2]), employeeData[3]);
-                log.info("Current Employee {}", employee);
-                if (employee.isBirthday(new XDate())) {
-                    log.info("Today is {} {}'s birthday", employee.firstName(), employee.lastName());
-                    employees.add(employee);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            log.error(e);
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            log.error(e);
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
+            URL resource = BirthdayLambdaAdapter.class.getResource("/employee_data.txt");
+            String filename = Paths.get(resource.toURI()).toFile().getAbsolutePath();
+
+            EmployeeRepository repository = new EmployeeCSVFileRepository(filename);
+            birthdayGreetingService = new BirthdayGreetingServiceImpl(repository, null);
+
+            List<Employee> employees = birthdayGreetingService.getListEmployeesWithBirthdayAt(LocalDate.now());
+            return EmployeeJSONModel.fromDomain(employees);
+
+        } catch (IOException | ParseException | URISyntaxException e) {
             log.error(e);
             throw new RuntimeException(e);
         }
-
-        return EmployeeJSONModel.fromDomain(employees);
     }
 }
